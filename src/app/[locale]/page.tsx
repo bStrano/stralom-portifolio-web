2995e76543e7b1a52ccf1bfe6c609bfc01bc7fd4
Bruno Strano <bruno_strano@hotmail.com>
2024-02-07 18:55:53 -0300

    🔧 Adding Firebase Analytics

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 import styles from '../page.module.css'
 import HomeIntroduction from "@/app/_components/HomeIntroduction";
 import React from "react";
+import '../../config/firebase.config'
 
 export default function Home() {
   return (
